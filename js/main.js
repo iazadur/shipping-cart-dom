7956a2c936d6ef32aa107bab3448ef1a20902216
iazadur <iamazadur@gmail.com>
2021-08-20 12:37:15 +0600

    selected

--- js/main.js
+++ js/main.js
@@ -11,15 +11,29 @@ const free = document.getElementById('free')
 const express = document.getElementById('express')
 
 const total = document.getElementById('total')
-const shipping =  document.getElementById('shipping')
+const shipping = document.getElementById('shipping')
+
+let isContain = function (elem) {
+    return elem.classList.contains('selected')
+}
 
 div1.addEventListener('click', function () {
+    if (!isContain(div1)) {
+        div1.classList.add('selected')
+        div2.classList.remove('selected')
+        div3.classList.remove('selected')
+    }
     img.src = 'images/1.jpg'
     description.innerText = 'first - Lorem ipsum dolor sit amet consectetur, adipisicing elit. Rerum beatae laboriosam, commodi quis tempore necessitatibus. Voluptatibus minima aut non doloremque?'
     price.innerText = '750'
     total.innerText = updateTotal()
 })
 div2.addEventListener('click', function () {
+    if (!isContain(div2)) {
+        div1.classList.remove('selected')
+        div2.classList.add('selected')
+        div3.classList.remove('selected')
+    }
     img.src = 'images/2.jpg'
     description.innerText = 'second - Lorem ipsum dolor sit amet consectetur, adipisicing elit. Rerum beatae laboriosam, commodi quis tempore necessitatibus. Voluptatibus minima aut non doloremque?'
     price.innerText = '650'
@@ -27,6 +41,11 @@ div2.addEventListener('click', function () {
 
 })
 div3.addEventListener('click', function () {
+    if (!isContain(div2)) {
+        div1.classList.remove('selected')
+        div2.classList.remove('selected')
+        div3.classList.add('selected')
+    }
     img.src = 'images/3.jpg'
     description.innerText = 'thired - Lorem ipsum dolor sit amet consectetur, adipisicing elit. Rerum beatae laboriosam, commodi quis tempore necessitatibus. Voluptatibus minima aut non doloremque?'
     price.innerText = '550'
@@ -35,9 +54,17 @@ div3.addEventListener('click', function () {
 })
 
 free.addEventListener('click', function () {
+    if (!isContain(free)) {
+        free.classList.add('selected')
+        express.classList.remove('selected')
+    }
     document.getElementById('shipping').innerText = 5
 })
 express.addEventListener('click', function () {
+    if (!isContain(express)) {
+        free.classList.remove('selected')
+        express.classList.add('selected')
+    }
     document.getElementById('shipping').innerText = 30
 })
 
